fc30731046177fad7a6d834bb73d33c19c5899b3
bart <barthand@gmail.com>
2011-05-14 14:35:12 +0200

    Added sqlite_creater.sh script which is generating the SQLite script for creating the Android-compatible DB purpose.

---
+++ scripts/sqlite_creater.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# This script generates the SQLite SQL script from the input data-file created by geoconverter.sh,
+# which can be used to generate the Android-compatible SQLite DB to be deployed along with the application.
+#
+# Author: barthand <barthand@gmail.com>
+#
+
+if [ -z $1 ] || [ ! -e $1 ]; then
+	echo "Please provide proper input file."
+	exit 1
+fi
+
+input_file=$1
+
+# First let's create the required DB schema.
+cat << EOF
+DROP TABLE IF EXISTS android_metadata;
+DROP TABLE IF EXISTS facility;
+
+CREATE TABLE android_metadata ("locale" TEXT DEFAULT 'en_US');
+INSERT INTO android_metadata VALUES ('en_US');
+
+CREATE TABLE facility (
+	_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	address TEXT,
+	phone TEXT,
+	email TEXT,
+	longitude REAL,
+	latitude REAL);
+			
+EOF
+
+# Now let's get the real data.
+awk -F'|' '
+{
+	if (length($5) == 0) lng="NULL"; else lng=$5;
+	if (length($6) == 0) lat="NULL"; else lat=$6;
+
+	print "INSERT INTO facility (name, address, phone, email, longitude, latitude) VALUES (\047"$1"\047, \047"$2"\047, \047"$3"\047, \047"$4"\047, "lng", "lat");"
+}' $input_file
